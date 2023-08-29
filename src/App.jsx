940b2dd495c419c9d309f75d978a6fefb1073f1f
Hitesh Malik <malik.hitu90@gmail.com>
2023-08-29 17:27:20 +0530

    feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react"
 import ImageCard from "./ImageCard"
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch} from "react-redux"
+import { imagedataUpdate } from "./Redux/slices/imageSlice"
 
 export default function App() {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const [search, setSearch] = useState('nature')
-
-  const disptatch = useDispatch()
+  const dispatch = useDispatch();
 
   const apicall = async (search) => {
     setLoading(true)
     try {
       await fetch(`https://api.pexels.com/v1/search?query=${search}`, {
         headers: {
-          Authorization: "PIafKSaxSZ6G0ael4mngs8bkrb4cyhkjPSqAhzSIdMUJ6yEyVgE45GUVS",
+          Authorization: "YyEpvYgvW89Usq3giXXqmqiqYtKVS8z70mzniSS0JsFAJ3635IavupuJ",
         }
       }).then((res) => {
         return res.json()
       }).then((data) => {
         setData(data.photos)
-        disptatch(ImagedataUpdate(data.photos))
+        dispatch(imagedataUpdate(data.photos))
       }
       )
     } catch (err) {
@@ -63,11 +63,11 @@ export default function App() {
               {
                 data?.length
                   ? (
-                    <div class="grid grid-cols-4 grid-rows-5 gap-8">
+                    <div className="grid grid-cols-4 grid-rows-5 gap-8">
                       {
-                        data.map((element) => {
+                        data.map((element,index) => {
                           return (
-                            <ImageCard data={element} />
+                            <ImageCard key={index} data={element} />
                           )
                         })
                       }
--- src/Redux/Store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { ImageSlice } from './slices/ImageData'
-
-export const store = configureStore({
-  reducer: {
-    ImageSlice : ImageSlice
-  },
-})
